fe8649644e58cf359d1e9ede97470b06fbccfdbd
Marcelo Junqueira <marcelojunqueiraf@gmail.com>
2025-04-19 13:37:41 -0300

    melhora swagger

--- src/ingredientes/ingredientes.controller.ts
+++ src/ingredientes/ingredientes.controller.ts
@@ -2,11 +2,25 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { IngredientesService } from './ingredientes.service';
 import { CreateIngredienteDto } from './dto/create-ingrediente.dto';
 import { UpdateIngredienteDto } from './dto/update-ingrediente.dto';
+import { ApiBody } from '@nestjs/swagger';
 
 @Controller('ingredientes')
 export class IngredientesController {
   constructor(private readonly ingredientesService: IngredientesService) {}
 
+  @ApiBody({
+    type: CreateIngredienteDto,
+    examples: {
+      exemplo1: {
+        summary: 'Exemplo de Ingrediente',
+        description: 'Um exemplo de ingrediente com nome e unidade',
+        value: {
+          nome: 'Farinha de Trigo',
+          unidade: 'kg',
+        },
+      },
+    },
+  })
   @Post()
   create(@Body() createIngredienteDto: CreateIngredienteDto) {
     return this.ingredientesService.create(createIngredienteDto);
@@ -22,8 +36,25 @@ export class IngredientesController {
     return this.ingredientesService.findOne(+id);
   }
 
+  @ApiBody({
+    type: UpdateIngredienteDto,
+    examples: {
+      exemplo1: {
+        summary: 'Exemplo de Atualização de Ingrediente',
+        description:
+          'Um exemplo de atualização de ingrediente com nome e unidade',
+        value: {
+          nome: 'Farinha de Trigo Integral',
+          unidade: 'kg',
+        },
+      },
+    },
+  })
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateIngredienteDto: UpdateIngredienteDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateIngredienteDto: UpdateIngredienteDto,
+  ) {
     return this.ingredientesService.update(+id, updateIngredienteDto);
   }
 
--- src/receitas/receitas.controller.ts
+++ src/receitas/receitas.controller.ts
@@ -3,11 +3,25 @@ import { ReceitasService } from './receitas.service';
 import { CreateReceitaDto } from './dto/create-receita.dto';
 import { UpdateReceitaDto } from './dto/update-receita.dto';
 import { AddIngredienteDto } from './dto/add-ingrediente.dto';
+import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('receitas')
 export class ReceitasController {
   constructor(private readonly receitasService: ReceitasService) {}
 
+  @ApiBody({
+    type: CreateReceitaDto,
+    examples: {
+      exemplo1: {
+        summary: 'Exemplo de Receita',
+        description: 'Uma receita de bolo de chocolate',
+        value: {
+          nome: 'Bolo de Chocolate',
+          descricao: 'Um delicioso bolo de chocolate.',
+        },
+      },
+    },
+  })
   @Post()
   create(@Body() createReceitaDto: CreateReceitaDto) {
     return this.receitasService.create(createReceitaDto);
@@ -23,6 +37,19 @@ export class ReceitasController {
     return this.receitasService.findOne(+id);
   }
 
+  @ApiBody({
+    type: UpdateReceitaDto,
+    examples: {
+      exemplo1: {
+        summary: 'Exemplo de Atualização de Receita',
+        description: 'Atualiza a receita de bolo de chocolate',
+        value: {
+          nome: 'Bolo de Chocolate com Morango',
+          descricao: 'Um delicioso bolo de chocolate com morangos.',
+        },
+      },
+    },
+  })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateReceitaDto: UpdateReceitaDto) {
     return this.receitasService.update(+id, updateReceitaDto);
@@ -33,13 +60,28 @@ export class ReceitasController {
     return this.receitasService.remove(+id);
   }
 
+  @ApiBody({
+    type: AddIngredienteDto,
+    examples: {
+      exemplo1: {
+        summary: 'Adicionar Ingrediente',
+        value: { ingredienteId: 1, quantidade: 2 },
+      },
+    },
+  })
   @Post(':id/ingredientes')
-  addIngredientes(@Param('id') id: string,
-  @Body() addIngredienteDto: AddIngredienteDto) {
+  addIngredientes(
+    @Param('id') id: string,
+    @Body() addIngredienteDto: AddIngredienteDto,
+  ) {
     return this.receitasService.addIngredientes(+id, addIngredienteDto);
   }
+
   @Delete(':id/ingredientes/:ingredienteId')
-  removeIngrediente(@Param('id') id: string, @Param('ingredienteId') ingredienteId: string) {
+  removeIngrediente(
+    @Param('id') id: string,
+    @Param('ingredienteId') ingredienteId: string,
+  ) {
     return this.receitasService.removeIngrediente(+id, +ingredienteId);
   }
 }
